c1da9fb13af5523c6e023d5a7326efb874fa883f
aetherscode <aetherscode@gmail.com>
2024-06-04 01:50:20 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ global.creator = 'AethersCode'// yourname
 global.MONGO_DB_URI = "mongodb+srv://aetherscode:aetherscode@aetherscode.nbmaobg.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "aetherscode@gmail.com" //email
-global.email_password = "@Fahadd17" //application password email
+global.email_password = "041711" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {

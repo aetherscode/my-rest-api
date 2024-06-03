13fc21ae7b3bc6d86283a641e9826a08548c10a2
Fahadd Still Learning <122947712+yushafahad17@users.noreply.github.com>
2024-06-03 13:46:04 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,11 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'AethersCode'// yourname
+global.MONGO_DB_URI = "mongodb+srv://aetherscode:aetherscode@aetherscode.nbmaobg.mongodb.net/?retryWrites=true&w=majority&appName
+" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "aetherscode" //email
+global.email_password = "@Fahadd17" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
